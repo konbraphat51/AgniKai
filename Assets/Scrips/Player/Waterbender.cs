1b996f7ef703eb940b560d91010a3f19d86b1e84
RI0501 <brightray48@zeus.eonet.ne.jp>
2022-07-09 13:28:13 +0900

    change repository name

--- Assets/Scrips/Player/Waterbender.cs
+++ Assets/Scrips/Player/Waterbender.cs
@@ -1,14 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
+using Common;
 using UnityEngine;
 
 public class Waterbender : Player
 {
     [SerializeField] private string aniCshoot = "BasicShoot";
+    [SerializeField] private string aniRunning = "Running";
+
+    [SerializeField] private float runningSpeed = 5.0f;
 
     public enum State
     {
-        waiting,
+        standing,
+        running,
         Cshoot
     }
     public State state
@@ -16,23 +19,115 @@ public class Waterbender : Player
         //cannot be changed by outsider class
         private set;
         get;
-    } = State.waiting;
+    } = State.standing;
+
+    private void Run(Directions direction)
+    {
+        //animation
+        animator.SetBool(aniRunning, true);
+
+        //move
+        switch (direction)
+        {
+            case Directions.right:
+                transform.position += new Vector3(runningSpeed, 0, 0);
+                break;
+            case Directions.left:
+                transform.position += new Vector3(-runningSpeed, 0, 0);
+                break;
+        }
+    }
 
     private void StartCshoot()
     {
-        Debug.Log("Cshoot");
         animator.SetTrigger(aniCshoot);
+    }
+
+    private bool CanMove()
+    {
+        switch (state)
+        {
+            case State.standing:
+            case State.running:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private void ChangeState(State nextState)
+    {
+        //only when different state
+        if (state == nextState) return;
+
+        //process for departing from current state
+        switch (state)
+        {
+            case State.running:
+                //animation
+                animator.SetBool(aniRunning, false);
+                break;
+        }
+
+        state = nextState;
     }
 
     protected override void OnKeyDownC()
     {
         base.OnKeyDownC();
 
-        switch (state)
+        //Cshoot
+        if (CanMove())
         {
-            case State.waiting:
-                StartCshoot();
-                break;
+            StartCshoot();
+            ChangeState(State.Cshoot);
+        }
+    }
+
+    protected override void OnKeyPushingA()
+    {
+        base.OnKeyPushingA();
+
+        //Run
+        if (CanMove())
+        {
+            Debug.Log("a");
+            Run(Directions.left);
+            ChangeState(State.running);
+        }
+    }
+
+    protected override void OnKeyPushingD()
+    {
+        base.OnKeyPushingD();
+
+        //Run
+        if (CanMove())
+        {
+            Run(Directions.right);
+            ChangeState(State.running);
+        }
+    }
+
+    protected override void OnKeyUpA()
+    {
+        base.OnKeyUpA();
+
+        //stop running
+        if(state == State.running)
+        {
+            ChangeState(State.standing);
+        }
+    }
+
+    protected override void OnKeyUpD()
+    {
+        base.OnKeyUpD();
+
+        //stop running
+        if (state == State.running)
+        {
+            ChangeState(State.standing);
         }
     }
 }
